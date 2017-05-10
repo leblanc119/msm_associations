ab6037a1209ff8e55cf4c40538755962d85ddb3b
leblanc119 <leblanc119@gmail.com>
2017-05-10 16:49:03 -0500

    Added some relationships and forms to actor to add character

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,7 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: { scope: :year }
   validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1870, less_than_or_equal_to: 2050 }
   validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2764800 }
-  
+
+  belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
+
 end
