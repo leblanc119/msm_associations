3e588b2001b5d03c2255911c9fa31fdf52d92548
leblanc119 <leblanc119@gmail.com>
2017-05-12 09:22:05 -0500

    Revert "Revert "Update validation""
    This reverts commit da0c299f8fea049b491d819007070c624a58ab40.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
   validates :director_id, :title, :year, :duration, presence: true
   validates :title, uniqueness: { scope: :year }
   validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1870, less_than_or_equal_to: 2050 }
-  validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2764800 }
+  validates :duration, numericality: { only_integer: true, :allow_blank => true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2764800 }
 
   belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
 
